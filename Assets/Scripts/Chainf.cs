37ebb676684caf38ac27e6c479090340a15bc12b
pssyuu <good9756@gmail.com>
2023-11-23 16:19:44 +0900

    23.11.23

--- Assets/Scripts/Chainf.cs
+++ Assets/Scripts/Chainf.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Chainf : MonoBehaviour
 {
     public GameObject ChaInform;
+    public Scriptable_NPC [] npc;
     bool OnOff = true;
 
-    public void ChainforOnOff()
+    int i = 0;
+
+    public void ChaInformOnOff()
     {
         if (OnOff)
         {
@@ -19,6 +23,23 @@ public class Chainf : MonoBehaviour
             ChaInform.SetActive(false);
             OnOff = true;
         }
+    }
 
+    public void NextChaInform()
+    {
+        if (i > npc.Length - 2)
+        {
+            i = 0;
+        }
+        else
+        {
+            i++;
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        ChaInform.GetComponentInChildren<Text>().text = "Name: " + npc[i].npcName + "\n" + "Max Health: " + npc[i].maxHealth + "\n" + "Level: " + npc[i].level + "\n" + "Speed: " + npc[i].speed + "\n" + "Agility: " + npc[i].agility;
     }
 }
